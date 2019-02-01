232db626e27a9b169a27ae1142422b68e99c4c27
Jon Miren Aresti Olin <jon.m.h.aresti@gmail.com>
2019-02-01 23:06:02 +0100

    feat: installed sort-by escape-string-regexp

--- src/SearchBook.js
+++ src/SearchBook.js
@@ -1,23 +1,39 @@
 import React, {Component} from "react"
 import {Link} from "react-router-dom"
-import BookList from "./BookList";
+import * as BooksAPI from './BooksAPI'
+import escapeRegExp from 'escape-string-regexp' 
+ import sortBy from 'sort-by'
+import BookList from "./BookList"
+
 
 class SearchBook extends Component {
+    state={
+        results:[],
+        query:""
+    }
+
+    search=()=>{
+        BooksAPI.search()
+    }
+
+    updateQuery=(query)=>{
+        this.setState({query: query.trim()})
+    }
+
     render(){
         return(
             <div className="search-books">
                 <div className="search-books-bar">
                     <Link to="/"><button className="close-search">Close</button></Link>
                     <div className="search-books-input-wrapper">
-                        {/*
-                        NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                        You can find these search terms here:
-                        https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
 
-                        However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                        you don't find a specific author or title. Every search is limited by search terms.
-                        */}
-                    <input type="text" placeholder="Search by title or author"/>
+                    <input 
+                    type="text" 
+                    placeholder="Search by title or author"
+                    value={this.state.query}
+                    onChange={(e)=> {this.updateQuery(e.target.value)
+                    this.setState({query:e.target.value})}
+                    }/>
                 </div>
             </div>
             <div className="search-books-results">
