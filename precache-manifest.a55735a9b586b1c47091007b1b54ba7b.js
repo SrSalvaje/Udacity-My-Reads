fa3bebc44d2cfd3b70b90c9fd79b32dba517dd2b
Jon Miren Aresti Olin <jon.m.h.aresti@gmail.com>
2019-03-18 11:52:27 +0100

    Updates

---
+++ precache-manifest.a55735a9b586b1c47091007b1b54ba7b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "9d4e3f366f039249695de85a30a02b95",
+    "url": "/Udacity-My-Reads/static/media/arrow-drop-down.9d4e3f36.svg"
+  },
+  {
+    "revision": "20e8847d1eb7759c19b0e30beb8849cf",
+    "url": "/Udacity-My-Reads/static/media/arrow-back.20e8847d.svg"
+  },
+  {
+    "revision": "75d390e5eabc3254418e950df631389b",
+    "url": "/Udacity-My-Reads/static/media/add.75d390e5.svg"
+  },
+  {
+    "revision": "0179db686df445a1b243",
+    "url": "/Udacity-My-Reads/static/js/runtime~main.0179db68.js"
+  },
+  {
+    "revision": "946e66dac6c33be8ffa1",
+    "url": "/Udacity-My-Reads/static/js/main.946e66da.chunk.js"
+  },
+  {
+    "revision": "a87b3d396533adb0430b",
+    "url": "/Udacity-My-Reads/static/js/1.a87b3d39.chunk.js"
+  },
+  {
+    "revision": "946e66dac6c33be8ffa1",
+    "url": "/Udacity-My-Reads/static/css/main.8b38a333.chunk.css"
+  },
+  {
+    "revision": "c83d21fbab333afeca9e5c66631963da",
+    "url": "/Udacity-My-Reads/index.html"
+  }
+];
